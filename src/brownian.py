b8e5fcfbb391e286ed234fb8b83408c5de8a79ec
Oracle Public Cloud User <opc@tutzing.subnet07191520.vcn07191520.oraclevcn.com>
2025-08-17 10:05:10 +0000

    Add Brownian motion implementation

---
+++ src/brownian.py
@@ -0,0 +1,25 @@
+"""
+brownian() implements one dimensional Brownian motion (i.e. the Wiener process).
+"""
+
+# File: brownian.py
+
+from math import sqrt
+from scipy.stats import norm
+import numpy as np
+
+
+def brownian(x0, n, dt, delta, out=None):
+    """
+    Generate an instance of Brownian motion (i.e. the Wiener process):
+
+        X(t) = X(0) + N(0, delta**2 * t; 0, t)
+
+    where N(a,b; t0, t1) is a normally distributed random variable with mean a and
+    variance b.  The parameters t0 and t1 make explicit the statistical
+    independence of N on different time intervals; that is, if [t0, t1) and
+    [t2, t3) are disjoint intervals, then N(a, b; t0, t1) and N(a, b; t2, t3)
+    are independent.
+    
+    Written as an iteration scheme,
+
