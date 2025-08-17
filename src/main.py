03d99f7eba7f05471dd3922928ecdfe5e9109351
Oracle Public Cloud User <opc@tutzing.subnet07191520.vcn07191520.oraclevcn.com>
2025-08-17 10:05:12 +0000

    Add main simulation imports and constants

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import math
+import numpy
+import matplotlib.pyplot as plt
+import brownian as bm
+import random
+"""
+High-frequency trading in a limit order book, Marco Avellaneda & Sasha Stoikov
+paper url: https://www.researchgate.net/publication/24086205_High_Frequency_Trading_in_a_Limit_Order_Book
+
+Some model limitations, discussed: https://quant.stackexchange.com/questions/36400/avellaneda-stoikov-market-making-model
+Parameter fitting: https://quant.stackexchange.com/questions/36073/how-does-one-calibrate-lambda-in-a-avellaneda-stoikov-market-making-problem
+"""
